e6bbd8b9b8381a94d314377dcc47d1682395b5c2
Fizzled <Fizzled04@gmail.com>
2024-05-01 15:21:05 -0500

    initial settings/main files

---
+++ tetris/main.py
@@ -0,0 +1,9 @@
+from settings import *
+
+class Main:
+    def __init__(self):
+        pygame.init()
+        self.display_surface = pygame.display.set_mode((WINDOW_WIDTH, WINDOW_HEIGHT))
+
+if __name__ == '__main__':
+    main = Main()

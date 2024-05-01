963e8a53e3a5d2f91d92469b73ef0e5528e8fc4f
Fizzled <Fizzled04@gmail.com>
2024-05-01 15:30:58 -0500

    Initial window done

--- tetris/main.py
+++ tetris/main.py
@@ -4,6 +4,21 @@ class Main:
     def __init__(self):
         pygame.init()
         self.display_surface = pygame.display.set_mode((WINDOW_WIDTH, WINDOW_HEIGHT))
+        self.clock = pygame.time.Clock()
+        pygame.display.set_caption('Tetris')
 
+    def run(self):
+        while True:
+            for event in pygame.event.get():
+                if event.type == pygame.QUIT:
+                    pygame.quit()
+                    exit()
+            
+            self.display_surface.fill(GREY)
+            
+            pygame.display.update()
+            self.clock.tick()
+            
 if __name__ == '__main__':
     main = Main()
+    main.run()
